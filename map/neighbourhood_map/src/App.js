04b7ea1e2586c38dd0755418814f574f5bc9cf9c
Dende <alegrria@gmail.com>
2018-12-18 11:39:13 +0100

    added function for displaying a map

--- map/neighbourhood_map/src/App.js
+++ map/neighbourhood_map/src/App.js
@@ -1,28 +1,44 @@
+/*global google*/
+/*thank you https://www.youtube.com/watch?v=W5LhLZqj76s*/
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  componentDidMount(){
+    this.loadMap();
+    window.initMap = this.initMap
+  }
+
+  loadMap = () => {
+    createScript('https://maps.googleapis.com/maps/api/js?key=AIzaSyCGxJxfJmNOm9iN23fX-nt4O0Sg762ByZU&v=3&callback=initMap');
+  }
+
+  initMap = () => {
+    let map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: -34.397, lng: 150.644},
+      zoom: 8
+    });
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div id='map'>
+
+        </div>
       </div>
     );
   }
+}
+
+ function createScript(url) {
+  let index = window.document.getElementsByTagName("script")[0];
+  let script = window.document.createElement("script");
+  script.src = url;
+  script.async = true;
+  script.defer = true;
+  index.parentNode.insertBefore(script, index)
 }
 
 export default App;
