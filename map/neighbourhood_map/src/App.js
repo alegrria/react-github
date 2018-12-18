3b6ca3cdf126c191235609eedc8ba48ff299d370
Dende <alegrria@gmail.com>
2018-12-18 12:31:54 +0100

    added geolocation

--- map/neighbourhood_map/src/App.js
+++ map/neighbourhood_map/src/App.js
@@ -1,5 +1,9 @@
 /*global google*/
-/*thank you https://www.youtube.com/watch?v=W5LhLZqj76s*/
+/*
+thank you
+Elharony for thorough explanation why initMap doesn't work straight https://www.youtube.com/watch?v=W5LhLZqj76s
+https://github.com/tomchentw/react-google-maps/issues/434 for solving google not defined error
+*/
 import React, { Component } from 'react';
 import './App.css';
 
@@ -16,10 +20,26 @@ class App extends Component {
 
   initMap = () => {
     let map = new google.maps.Map(document.getElementById('map'), {
-      center: {lat: -34.397, lng: 150.644},
+      center: {lat: 53.551086, lng: 9.993682},
       zoom: 8
     });
+    let infoWindow = new google.maps.InfoWindow();
+
+    // Try HTML5 geolocation.
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        var pos = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        };
+        infoWindow.setPosition(pos);
+        infoWindow.setContent('Location found.');
+        infoWindow.open(map);
+        map.setCenter(pos);
+      })
+    }
   }
+
 
   render() {
     return (
@@ -33,12 +53,13 @@ class App extends Component {
 }
 
  function createScript(url) {
-  let index = window.document.getElementsByTagName("script")[0];
-  let script = window.document.createElement("script");
-  script.src = url;
-  script.async = true;
-  script.defer = true;
-  index.parentNode.insertBefore(script, index)
-}
+    let index = window.document.getElementsByTagName("script")[0];
+    let script = window.document.createElement("script");
+    script.src = url;
+    script.async = true;
+    script.defer = true;
+    index.parentNode.insertBefore(script, index)
+  }
+
 
 export default App;
