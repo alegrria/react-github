c972068945158f072d2101ed326a8cce6fd3369b
Dende <alegrria@gmail.com>
2018-12-19 13:53:31 +0100

    rendered cuisine options for select

--- map/neighbourhood_map/src/Footer.js
+++ map/neighbourhood_map/src/Footer.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Footer = (props) => {
 
+  Footer.propTypes = {
+    cuisines: PropTypes.array.isRequired,
+    results: PropTypes.array.isRequired
+  }
+
   return (
+
     <div id="footer">
       <h2>Listings</h2>
       <select id="cuisines-select" name="cuisines" aria-label="selection of cuisines" onChange="updateRestaurants()">
-          <option value={props.cuisines}>All Cuisines</option>
-          <option value="all">All Cuisines</option>
+          {props.results.map(cuisine => <option key={cuisine} value={props.cuisines.find(x => x.id === cuisine).name}>{props.cuisines.find(x => x.id === cuisine).name}</option>)}
       </select>
     </div>
   )
