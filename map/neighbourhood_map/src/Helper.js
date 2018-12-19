d7a53b72ef3b3c772ca6887cd482e65bd8f4e5cb
Dende <alegrria@gmail.com>
2018-12-19 10:46:39 +0100

    increased limit of returned results

--- map/neighbourhood_map/src/Helper.js
+++ map/neighbourhood_map/src/Helper.js
@@ -1,4 +1,4 @@
-const apiCall = 'https://api.foursquare.com/v2/venues/search?client_id=O1W14F1AL2SS5JIKXXYMTOHERT0IV4AFKEVJGI3J2JNOJTVL&client_secret=O3PYRRN0HJOE0WWJ3Z2BL0UQBUJMLZ1NPVLSZQCNAI1WXXMC&v=20180323&limit=10&ll=53.54872,9.97855&query=restaurant'
+const apiCall = 'https://api.foursquare.com/v2/venues/search?client_id=O1W14F1AL2SS5JIKXXYMTOHERT0IV4AFKEVJGI3J2JNOJTVL&client_secret=O3PYRRN0HJOE0WWJ3Z2BL0UQBUJMLZ1NPVLSZQCNAI1WXXMC&v=20180323&limit=20&ll=53.54872,9.97855&query=restaurant'
 
 export function venues() {
   return fetch(apiCall)
