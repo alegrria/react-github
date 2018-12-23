2d959f98375048b16e7670049cb03fcf026ae2e0
Dende <alegrria@gmail.com>
2018-12-23 23:10:26 +0100

    used flex to style map and footer

--- map/neighbourhood_map/src/App.js
+++ map/neighbourhood_map/src/App.js
@@ -93,8 +93,10 @@ class App extends Component {
     return (
       <div className="App">
         <Header/>
-        <div id='map' aria-label="restaurant location map"></div>
-        <Footer restos={this.state.allVenues} results={Helper.uniqueCategories(this.state.cuisines)} updateRestaurants={this.updateRestaurants} selection={this.state.venues} markers={this.state.markers}/>
+        <div class="flexbox-container">
+          <Footer restos={this.state.allVenues} results={Helper.uniqueCategories(this.state.cuisines)} updateRestaurants={this.updateRestaurants} selection={this.state.venues} markers={this.state.markers}/>
+          <div id='map' aria-label="restaurant location map"></div>
+        </div>
       </div>
     );
   }
