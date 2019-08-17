c0538d0dfaa63d08e558e658d250a8eb924931c2
Dende <alegrria@gmail.com>
2019-08-17 17:23:14 +0200

    added component for list of users

--- react-github/src/SearchField.js
+++ react-github/src/SearchField.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
+import UserList from './UserList'
 
 Class Search extends Component {
 
+  render() {
+    return
+  }
 }
 
 export default Search
---
+++ react-github/src/UserList.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import User from './User'
+
+Class UserList extends Component {
+
+  render() {
+    return
+  }
+}
+
+export default UserList
