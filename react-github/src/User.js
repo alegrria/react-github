bdd2307e4532b8db6c210f947bf469f074d35b6e
Dende <alegrria@gmail.com>
2019-08-17 17:43:52 +0200

    added structure of a user component

--- react-github/src/User.js
+++ react-github/src/User.js
@@ -1,7 +1,22 @@
 import React, { Component } from 'react';
 
-Class User extends Component {
+Class User = (props) => {
+  User.propTypes = {
+    name: PropTypes.object.isRequired,
+    username: PropTypes.object.isRequired,
+    avatar_url: PropTypes.object.isRequired,
+    followers_count: PropTypes.object.isRequired,
+  };
 
-}
+  return
+    <li key={props.username} >
+      <div className="user">
+        <div className="user-name">Name is {props.name}</div>
+        <div className="user-username">Username is {props.username}</div>
+        <div className="user-username">See the avatar: {props.avatar_url}</div>
+        <div className="user-username">Floowed by: {props.followers_count}</div>
+      </div>
+    </li>
+};
 
 export default User
