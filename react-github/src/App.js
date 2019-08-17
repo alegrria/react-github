36a0978504c3172725f22a730e76992a3220abd5
Dende <alegrria@gmail.com>
2019-08-17 17:55:43 +0200

    structured application

--- react-github/src/App.js
+++ react-github/src/App.js
@@ -1,17 +1,27 @@
 import React, { Component } from 'react';
+import SearchField from './SearchField';
+import UserList from './UserList';
+import User from './User';
 import './App.css';
 
 class App extends Component {
   state = {
     users: []
   }
-
-
+  componentDidMount(){
+      GitHubAPI.getUsers().then((users) => {
+        this.setState({
+          users
+        })
+      })
+    }
 
   render() {
     return (
-      <main className="App">
-      </main>
+      <div className="App">
+        <div className="search"><SearchField/></div>
+        <div className="search"><UserList/></div>
+      </div>
     );
   }
 }
